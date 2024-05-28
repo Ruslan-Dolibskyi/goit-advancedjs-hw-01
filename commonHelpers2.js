824227ebd649003cd4875fcda8f711fb02625454
Ruslan-Dolibskyi <dolibskiyruslan@gmail.com>
2024-05-28 04:42:54 +0000

    Deploying to gh-pages from @ Ruslan-Dolibskyi/goit-advancedjs-hw-01@c992679d07fe3ecdb397bf523b636ffdb5d06d8e 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */import{P as r,t as a}from"./assets/vendor-1e54b975.js";const n=document.querySelector("iframe"),e=new r(n),t="videoplayer-current-time",m=function(o){localStorage.setItem(t,o.seconds)};e.on("timeupdate",a(m,1e3));e.setCurrentTime(localStorage.getItem(t)||0);
+//# sourceMappingURL=commonHelpers2.js.map
